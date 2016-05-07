6cd456475062cb957cd7d2f4386f253cea60fcbe
jonjamison3 <jonjamison3@gmail.com>
2016-05-06 23:45:41 -0400

    bootstrapping with angular, beginning addition of some table usefulness

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var app = angular.module('ngAhoy', [])
+.controller('skillsCtl', function($scope){
+  $scope.jsSelected=false;
+  $scope.htmlSelected=false;
+  $scope.cssSelected=false;
+  $scope.bootstrapSelected=false;
+  $scope.jquerySelected=false;
+  $scope.phpSelected=false;
+  $scope.ngSelected=false;
+  $scope.nodeSelected=false;
+  $scope.npmSelected=false;
+  $scope.expressSelected=false;
+});
