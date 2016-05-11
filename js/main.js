734b8ca4a2bfaa4dc3476df2b6dc121752b6a8f4
jonjamison3 <jonjamison3@gmail.com>
2016-05-11 14:35:22 -0400

    segmenting a directive for table

--- js/main.js
+++ js/main.js
@@ -1,18 +1,26 @@
-var app = angular.module('ngAhoy', [])
-.controller('skillsCtl', function($scope){
-  $scope.jsSelected=false;
-  $scope.htmlSelected=false;
-  $scope.cssSelected=false;
-  $scope.bootstrapSelected=false;
-  $scope.jquerySelected=false;
-  $scope.phpSelected=false;
-  $scope.ngSelected=false;
-  $scope.nodeSelected=false;
-  $scope.npmSelected=false;
-  $scope.expressSelected=false;
-})
-.controller('contactCtl', function($scope){
+var app = angular.module('ngAhoy', []);
+
+app.controller('contactCtl', function($scope){
   $scope.twitter = false;
   $scope.github = "https://www.github.com/jonjamison3";
   $scope.google = false;
 });
+app.directive('skillsTable', [function($scope){
+  return {
+    restrict: 'E',
+    templateUrl:"skills-table.html",
+    controller: function(){
+      $scope.jsSelected=false;
+      $scope.htmlSelected=false;
+      $scope.cssSelected=false;
+      $scope.bootstrapSelected=false;
+      $scope.jquerySelected=false;
+      $scope.phpSelected=false;
+      $scope.ngSelected=false;
+      $scope.nodeSelected=false;
+      $scope.npmSelected=false;
+      $scope.expressSelected=false;
+    },
+    controllerAs:"skillsCtl"
+  }
+}]);
