ba3f4c9f633c9117428267298356b9486a20aac2
jonjamison3 <jonjamison3@gmail.com>
2016-05-11 22:45:03 -0400

    curbing some ambition for now

--- js/main.js
+++ js/main.js
@@ -1,26 +1,21 @@
-var app = angular.module('ngAhoy', []);
 
-app.controller('contactCtl', function($scope){
-  $scope.twitter = false;
-  $scope.github = "https://www.github.com/jonjamison3";
-  $scope.google = false;
-});
-app.directive('skillsTable', [function($scope){
-  return {
-    restrict: 'E',
-    templateUrl:"skills-table.html",
-    controller: function(){
-      $scope.jsSelected=false;
-      $scope.htmlSelected=false;
-      $scope.cssSelected=false;
-      $scope.bootstrapSelected=false;
-      $scope.jquerySelected=false;
-      $scope.phpSelected=false;
-      $scope.ngSelected=false;
-      $scope.nodeSelected=false;
-      $scope.npmSelected=false;
-      $scope.expressSelected=false;
-    },
-    controllerAs:"skillsCtl"
-  }
-}]);
+  var app = angular.module('ngAhoy', []);
+
+  app.controller('contactCtl', function($scope){
+    $scope.twitter = false;
+    $scope.github = "https://www.github.com/jonjamison3";
+    $scope.google = false;
+  });
+  app.controller('skillsCtl', function($scope){
+    $scope.jsSelected=false;
+    $scope.htmlSelected=false;
+    $scope.cssSelected=false;
+    $scope.bootstrapSelected=false;
+    $scope.jquerySelected=false;
+    $scope.phpSelected=false;
+    $scope.ngSelected=false;
+    $scope.nodeSelected=false;
+    $scope.npmSelected=false;
+    $scope.expressSelected=false;
+  })
+  
